867ba544bc6a351fdee63ad209d9573f8caf0c81
Konstantin <lorcat@gmail.com>
2021-03-01 13:29:21 +0100

    added offline Raman configuration

---
+++ code/app/plugins/DEV_000F314C6B3A_plugin_01.py
@@ -0,0 +1,34 @@
+from .common.base_tango import TangoMover
+
+__all__ = ["get_version", "move_xy"]
+__version__ = "0.0.1"
+__description__ = "Raman stage YZ"
+
+
+def get_version():
+    """
+    Returns a version of a plugin - must be present
+    """
+    global __version__
+    return __version__
+
+
+def get_description():
+    """
+    Returns a short description
+    """
+    global __description__
+    return __description__
+
+
+def move_xy(*args):
+    """
+    Performes a movement - must be present
+    """
+    dx, dy = args
+    print("\nMoving by dx:dy ({}:{})".format(dx, dy))
+
+    # x, y, convx, convy - in frame coordinates x is horizontal, y is vertical
+    corr = 1e-4 * 4.075
+    m = TangoMover("haspllabcl1:10000/llab/motor/mot.02", "haspllabcl1:10000/llab/motor/mot.03", corr, corr, brealmove=True)
+    m.run(dx=dx, dy=dy)
